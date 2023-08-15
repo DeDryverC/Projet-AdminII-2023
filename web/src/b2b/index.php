3ab39008ca1f1892d376a6b80061f46cf579182d
Cédric De Dryver <c.dedryver@students.ephec.be>
2023-08-15 16:23:55 +0200

    [FIX] Fix the php script to show informations

--- web/src/b2b/index.php
+++ web/src/b2b/index.php
@@ -9,45 +9,68 @@
             <h3>Mettre un article en vente</h3>
             <form method="POST">
                 <label for="article">Article: </label>
-                <input type="text" name="article" placeholder="nom de l'article">
+                <input type="text" name="name" placeholder="nom de l'article">
+                <label for="brand">Brand: </label>
+                <input type="text" name="brand" placeholder="Nom de la marque">
                 <label for="prix">Prix: </label>
-                <input type="number" name="prix" placeholder="en euro">
-
+                <input type="number" step="0.01" name="price" placeholder="en euro">
                 <input type="submit" name="submit" value="Mettre en vente">
             </form>
 
         </div>
         <div>
             <h3>Articles en vente</h3>
-        <?php
-	    define("DB_HOST", "mysql");
-	    define("DB_USERNAME", "admin");
-	    define("DB_PASSWORD", "admin123");
-	    define("DB_NAME", "woodytoys");
-	    $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-	    
-	    if ($conn->connect_error){
-		 echo "An Error occured with the database, please contact the system administrator.";
-	   	    
-	    }
+            <table>
+                <thead>
+                    <th> ID </th>
+                    <th> Nom </th>
+                    <th> Marque </th>
+                    <th> Prix </th>
+                </thead>
+                <?php
+                define("DB_HOST", "mysql");
+                define("DB_USERNAME", "admin");
+                define("DB_PASSWORD", "admin123");
+                define("DB_NAME", "woodytoys");
+                $conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
                 
-        $id = 0;
-        if($result = mysqli_query($conn, "SELECT * FROM articles")) {
-            echo $row['name'] . " - " . $row['price'] . "€ <br />\n";
-            if($row["id"] > $id) {
-                $id = $row["id"];
-            }
-        }
+                if ($conn->connect_error){
+                    echo "An Error occured with the database, please contact the system administrator.";
+                    
+                }
+                        
+                $id = 0;
 
-        if (isset($_POST['submit'])){
-            $article = $_POST["name"];
-            $prix = $_POST["price"];
+                $sql = "SELECT * FROM articles";
+                $result = $conn->query($sql);
 
-            $insert = "INSERT INTO articles VALUES ('$article', $prix)";
-            $conn->exec($insert);
-            echo "<meta http-equiv='refresh' content='0'>";
-        }
-        ?>
+                if ($result->num_rows > 0) {
+                    printf('<tbody>');
+                    while($row = $result->fetch_assoc()) {
+                        printf("<tr><td>%d</td><td>%s</td><td>%s</td><td>%.2f</td></tr>", 
+                            $row["id"], 
+                            $row["name"], 
+                            $row["brand"],
+                            $row["price"]);               
+                    }
+                    printf('</tbody>');
+                } else {
+                    printf('No record found.<br />');
+                }
+
+                if (isset($_POST['submit'])){
+                    $new_article_id = $id + 1;
+                    $article = $_POST["name"];
+                    $prix = $_POST["price"];
+                    $brand = $_POST["brand"];
+
+                    $insert = "INSERT INTO articles VALUES ($id, '$article', '$brand', $prix)";
+                    mysqli_query($conn, $insert);
+                    echo "<meta http-equiv='refresh' content='0'>";
+                }
+                $conn->close();
+                ?>
+            </table>
         </div>
     </body>
 </html>
